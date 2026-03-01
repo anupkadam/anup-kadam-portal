b50e55e13cd4d6a6475fc68d0557ca072e339dbf
anupkadam <anupkadam01@gmail.com>
2026-03-01 18:23:29 +0530

    Update spiritual.js

--- js/spiritual.js
+++ js/spiritual.js
@@ -1,18 +1,5 @@
-fetch("data/content.json")
-.then(res => res.json())
-.then(data => {
-
-    let html = "";
-
-    data.spiritual.forEach(p => {
-        html += `
-        <div class="card p-3 shadow">
-            <h3>${p.title}</h3>
-            <p>${p.content}</p>
-            <a href="${p.link}" target="_blank" class="btn btn-success">Open Material</a>
-        </div>
-        `;
-    });
-
-    document.getElementById("spiritual").innerHTML = html;
-});
+html += `<div class="spiritual-card">
+    <h3>${item.title}</h3>
+    <p>${item.content}</p>
+    <a href="${item.link}" target="_blank" class="btn-accent-custom">Open Material</a>
+</div>`;
